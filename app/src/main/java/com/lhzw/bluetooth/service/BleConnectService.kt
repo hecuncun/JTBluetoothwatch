46eda4c5c3b4c605d739e24050d6b2441540398a
hecuncun <hecuncun>
2020-04-14 17:52:50 +0800

    bug

--- app/src/main/java/com/lhzw/bluetooth/service/BleConnectService.kt
+++ app/src/main/java/com/lhzw/bluetooth/service/BleConnectService.kt
@@ -166,7 +166,6 @@ class BleConnectService : Service() {
 
         override fun onBatchScanResults(results: MutableList<ScanResult>) {
             Log.e("SCANCallBack","搜索设备中...size==${results.size}")
-
             for (result in results) {
                 if (result.device.name != null && result.device.name.contains("SW2500")) {
                     if (mListValues.size == 0) {
