7da984cee975bb50dd61aed06aa936a4c0db7aa9
xtqb <l3455221w>
2020-06-18 08:51:27 +0800

    注销BLEActivity蓝牙扫描功能

--- app/src/main/java/com/lhzw/bluetooth/ui/activity/BLEWatchListActivity.kt
+++ app/src/main/java/com/lhzw/bluetooth/ui/activity/BLEWatchListActivity.kt
@@ -1,23 +1,12 @@
 package com.lhzw.bluetooth.ui.activity
 
-import android.Manifest
-import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.Handler
 import android.support.v7.widget.LinearLayoutManager
-import android.text.TextUtils
-import android.util.Log
 import com.lhzw.bluetooth.R
 import com.lhzw.bluetooth.adapter.WatchAdapter
 import com.lhzw.bluetooth.base.BaseActivity
 import com.lhzw.bluetooth.bean.ConnectWatchBean
 import com.lhzw.bluetooth.uitls.BaseUtils
-import com.lhzw.bluetooth.uitls.ToastUtils
 import com.lhzw.bluetooth.view.SpaceItemDecoration
 import com.lhzw.bluetooth.widget.LoadingView
 import kotlinx.android.synthetic.main.activity_ble_watchs.*
@@ -29,9 +18,10 @@ import kotlinx.android.synthetic.main.activity_ble_watchs.*
  * Created by xtqb.
  */
 class BLEWatchListActivity : BaseActivity() {
-    private val SCAN_DURATION: Long = 20000//扫描持续时长10s
+    //    private val SCAN_DURATION: Long = 20000//扫描持续时长10s
     private var watchList: MutableList<ConnectWatchBean>? = null
-    private var bleManager: BluetoothManager? = null
+
+    //    private var bleManager: BluetoothManager? = null
     private val PERMISS_REQUEST_BLE_CODE = 0X357
     private var loadingView: LoadingView? = null
     private val TAG = BLEWatchListActivity::class.java.simpleName
@@ -45,36 +35,41 @@ class BLEWatchListActivity : BaseActivity() {
     }
 
     override fun initData() {
+        /*
         bleManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         var state = false
         bleManager?.apply {
             state = adapter.isEnabled//拿到蓝牙状态
         }
+        */
+
         val manager = LinearLayoutManager(this)
         manager.orientation = LinearLayoutManager.VERTICAL
         recyclerView.layoutManager = manager
         watchList = ArrayList()
+        watchList?.add(ConnectWatchBean("JIANGTAI疆泰05X", "型号：SW2500", "应用领域：森林消防局", "规模搜救"))
+        watchList?.add(ConnectWatchBean("JIANGTAI疆泰05X", "型号：SW2501", "应用领域：森林消防局", "规模搜救"))
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(SpaceItemDecoration(BaseUtils.dip2px(16)))
 
-        if (!state) {
-            ToastUtils.toastError("蓝牙未开启, 请开启蓝牙")
-        } else {
-            loadingView = LoadingView(this)
-            loadingView?.setLoadingTitle("搜索中...")
-            if (checkPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION))) {
-                startScan()
-            } else {
-                requestPermission(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISS_REQUEST_BLE_CODE)
-            }
-        }
-        toothMap = HashMap()
+//        if (!state) {
+//            ToastUtils.toastError("蓝牙未开启, 请开启蓝牙")
+//        } else {
+//            loadingView = LoadingView(this)
+//            loadingView?.setLoadingTitle("搜索中...")
+//            if (checkPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION))) {
+//                startScan()
+//            } else {
+//                requestPermission(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISS_REQUEST_BLE_CODE)
+//            }
+//        }
+//        toothMap = HashMap()
     }
 
     override fun initView() {
 
     }
-
+    /*
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == PERMISS_REQUEST_BLE_CODE) {
@@ -146,7 +141,7 @@ class BLEWatchListActivity : BaseActivity() {
         }
 
     }
-
+    */
 
     override fun initListener() {
         im_back.setOnClickListener {
@@ -160,19 +155,19 @@ class BLEWatchListActivity : BaseActivity() {
             it.clear()
         }
         watchList = null
-        bleManager?.apply {
-            if (adapter.isDiscovering) {
-                adapter.cancelDiscovery()
-            }
-        }
-        unregisterReceiver(receiver)
-        bleManager = null
-        loadingView?.let {
-            if (it.isShowing) {
-                it.dismiss()
-            }
-        }
-        loadingView = null
+//        bleManager?.apply {
+//            if (adapter.isDiscovering) {
+//                adapter.cancelDiscovery()
+//            }
+//        }
+//        unregisterReceiver(receiver)
+//        bleManager = null
+//        loadingView?.let {
+//            if (it.isShowing) {
+//                it.dismiss()
+//            }
+//        }
+//        loadingView = null
     }
 
 }
