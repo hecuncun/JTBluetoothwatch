5ba7727a7c17f9aeda017525c1b49ad9a9a8b16b
xtqb <l3455221w>
2020-08-31 14:58:41 +0800

    修改检验对象字段，添加了passwod和verifyCode

--- app/src/main/java/com/lhzw/bluetooth/bean/MsgVerifyBean.kt
+++ app/src/main/java/com/lhzw/bluetooth/bean/MsgVerifyBean.kt
@@ -7,5 +7,10 @@ package com.lhzw.bluetooth.bean
  * @CreateDate: 2020/8/26 0026 16:55
  * @Description:
  *
+ *  说明：
+ *      获取检验码时，参数 ： verifyCode  和 参数 password 可以为null
+ *      修改密码时， 所有参数均不能为空 ！！！
+ *
+ *
  */
-data class MsgVerifyBean(val loginName: String, val tell: String)
+data class MsgVerifyBean(val loginName: String, val tell: String, val verifyCode: String?, val password: String?)
